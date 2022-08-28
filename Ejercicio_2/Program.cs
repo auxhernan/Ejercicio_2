3520ae9671225b4c4b844178155f94781d8a396b
Usuario <Usuario@DESKTOP-BIRK270>
2022-08-28 10:39:02 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+
+namespace Ejercicio_2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int a = Sumar(5, 6);
+            Console.WriteLine(a);
+         
+        }
+
+        public static int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
